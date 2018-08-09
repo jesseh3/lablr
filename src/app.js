87deed4e68aed85783f60d6361b654e05008b9ec
Jesse Hall <jesse.hall@infotechfl.com>
2018-08-09 14:24:34 -0400

    Added styles.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 var React = require('react');
+var styles = require('./styles/main.styl');
 
 var Hello = React.createClass({
   displayName: "Hello",
   render: function() {
-    return <div>Hello, {this.props.name}</div>
+    return <div><h1>Hello, {this.props.name}</h1></div>
   }
 })
 
