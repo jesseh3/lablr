f23249a5a76ead4568bed105715b88d927062153
Jesse Hall <jesse.hall@infotechfl.com>
2018-08-09 16:13:27 -0400

    Added more html and styles

--- src/pages/public.js
+++ src/pages/public.js
@@ -3,6 +3,18 @@ import React from 'react'
 export default React.createClass({
   displayName: 'PublicPage',
   render () {
-    return <h1>Public Page</h1>
+    return (
+      <div className='container'>
+        <header role='banner'>
+          <h1>Labelr</h1>
+        </header>
+        <div>
+          <p>We label stuff for you, because, we can&trade;</p>
+          <a href='/repos' className='button button-large'>
+            <span className='mega-octicon octicon-mark-github'></span> Login with GitHub
+          </a>
+        </div>
+      </div>
+    )
   }
 })
