3e20720ff02b09f80df3cd490424fe7d4043df17
Jesse Hall <jessehall3@gmail.com>
2018-08-12 09:11:00 -0400

    Login link now sending you to auth at github

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import app from 'ampersand-app'
 import React from 'react'
+import qs from 'qs'
 import Router from 'ampersand-router'
 import PublicPage from './pages/public'
 import ReposPage from './pages/repos'
@@ -20,13 +21,33 @@ export default Router.extend({
   routes: {
     '': "public",
     "repos": "repos",
+    "login": "login",
+    "logout": "logout",
+    "auth/callback": "authCallback"
   },
-  
+
   public () {
     this.renderPage(<PublicPage/>, {layout: false})
   },
-  
+
   repos (){
     this.renderPage(<ReposPage/>)
   },
+
+  login () {
+    window.location = "https://github.com/login/oauth/authorize?"
+    +  qs.stringify({
+      client_id: "a7ef50bbbe6cc8e4480c",
+      redirect_uri: window.location.origin + "/auth/callback",
+      scope: "user,repo"
+    })
+  },
+
+  logout () {
+
+  },
+
+  authCallback () {
+
+  }
 })
