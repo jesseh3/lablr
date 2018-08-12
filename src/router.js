2aa96bff5b756321b90353decbb8b023319c46e9
Jesse Hall <jessehall3@gmail.com>
2018-08-12 09:43:06 -0400

    Now sending and retrieving state (a random string) with auth requests.

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import app from 'ampersand-app'
 import React from 'react'
 import qs from 'qs'
+import uuid from 'node-uuid'
 import Router from 'ampersand-router'
 import PublicPage from './pages/public'
 import ReposPage from './pages/repos'
@@ -36,11 +37,14 @@ export default Router.extend({
   },
 
   login () {
+    const state = uuid()
+    window.localStorage.state = state
     window.location = "https://github.com/login/oauth/authorize?"
     +  qs.stringify({
       client_id: "a7ef50bbbe6cc8e4480c",
       redirect_uri: window.location.origin + "/auth/callback",
-      scope: "user,repo"
+      scope: "user,repo",
+      state: state
     })
   },
 
