d55cb36b53d5a0816264e88b9e0fd2a6f2d269ec
Jesse Hall <jessehall3@gmail.com>
2018-08-18 20:45:09 -0400

    Now will display list of repos after a manual fetch.

--- src/pages/repos.js
+++ src/pages/repos.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import ampersandMixin from 'ampersand-react-mixin'
 
 export default React.createClass({
+  mixins: [ampersandMixin],
   displayName: 'PublicPage',
   render () {
-    return <h1>Repos Page</h1>
+    const {repos} = this.props
+    return (
+      <div>
+        <h1>Repos Page</h1>
+        <div>
+          {repos.map((repo) => {
+              return (
+                <div>
+                  <a href="#" style={{fontSize: '45px'}}>
+                    {repo.full_name}
+                  </a>
+                </div>
+              )
+          })}
+        </div>
+      </div>
+    )
   }
 })
--- src/router.js
+++ src/router.js
@@ -34,7 +34,7 @@ export default Router.extend({
   },
 
   repos (){
-    this.renderPage(<ReposPage/>)
+    this.renderPage(<ReposPage repos={app.me.repos}/>)
   },
 
   login () {
