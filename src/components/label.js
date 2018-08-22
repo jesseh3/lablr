fd609cfed7dd6055cc87b173405554c12217b5dc
Jesse Hall <jessehall3@gmail.com>
2018-08-21 20:29:36 -0400

    Can click pencil and cancel button.

--- src/components/label.js
+++ src/components/label.js
@@ -1,13 +1,44 @@
 import React from 'react'
+import ampersandMixin from 'ampersand-react-mixin'
 
 export default React.createClass({
+  mixins: [ampersandMixin],
+
   displayName: 'Label',
+
+  onEditClick () {
+    this.props.label.editing = true
+  },
+
+  onCancelClick () {
+    this.props.label.editing = false
+  },
 
   render () {
     const {label} = this.props
+    let content
 
-    return (
-      <li>{label.name}</li>
-    )
+    if (label.editing) {
+      content = (
+        <form className='label'>
+          <span className='label-color avatar avatar-small avatar-rounded'>&nbsp;</span>
+          <input name='name'/>
+          <input name='color'/>
+          <button type='submit' className='button button-small'>Save</button>
+          <button onClick={this.onCancelClick} type='button' className='button button-small button-unstyled'>cancel</button>
+        </form>
+      )
+    } else {
+      content = (
+        <div className='label'>
+          <span className='label-color'>&nbsp;</span>
+          <span>{label.name}</span>
+          <span onClick={this.onEditClick} className='octicon octicon-pencil'></span>
+          <span className='octicon octicon-x'></span>
+        </div>
+      )
+    }
+
+    return <div>{content}</div>
   }
 })
